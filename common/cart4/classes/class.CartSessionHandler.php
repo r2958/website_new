5601b50fac5e5e987ca419e53308be23338fc985
andrew renwei <r2958@163.com>
2022-02-15 11:07:39 +0800

    update sessionHandler

--- common/cart4/classes/class.CartSessionHandler.php
+++ common/cart4/classes/class.CartSessionHandler.php
@@ -3,10 +3,10 @@
 require_once('./common/functions/class.Neturf.php');
 //require_once(Neturf::getServerRoot() . '/common/functions/class.SessionHandler.php');
 require_once('./common/functions/class.SessionHandler.php');
-class CartSessionHandler extends SessionHandlerA {
+class CartSessionHandler extends SessionHandler {
 
 	function CartSessionHandler() {
-		parent::SessionHandlerA();
+		parent::SessionHandler();
 	}
 
 	function gc($maxlifetime) {
