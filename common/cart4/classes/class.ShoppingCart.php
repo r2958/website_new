8592c698e5c51f74903fcc745f1886770c4c4992
andrew renwei <r2958@163.com>
2022-02-17 17:47:47 +0800

    update

--- common/cart4/classes/class.ShoppingCart.php
+++ common/cart4/classes/class.ShoppingCart.php
@@ -393,7 +393,7 @@ class ShoppingCart
 		$order->Fax = $frm['Fax'];
 		$order->Comments = $frm['Comments'];
 		$order->MailingList = $frm['MailingList'];
-		var_dump($order);exit;
+		var_dump($order);
 
 		$_SESSION['orderinfo'] = $order;
 
