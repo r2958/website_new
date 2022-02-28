ace8f0f2d2f491bd6df88fa1a7fbbf382347ef80
Ren wei <r2958@163.com>
2022-02-28 09:42:34 +0800

    update count function paramater

--- admin/products/update.php
+++ admin/products/update.php
@@ -3,7 +3,7 @@ require_once('../../application.php');
 
 if(isset($_POST['done']) && ($_POST['done'] == 'Yes')) {
 	if($_POST['ProductName'] == '') $errorList[] = 'Name field left blank.';
-	if(count($_POST['categories']) == 0) $errorList[] = 'No Category Selected.';
+	if(count((is_countable($_POST['categories'])?$_POST['categories']:[]))== 0) $errorList[] = 'No Category Selected.';
 	if(sizeof($errorList) > 0) $Admin->DisplayError($errorList);
 	
 	if($_POST['ProductID'] == '') {
